5d22cc40c4d2103a1aa35ed5df176b6f4024c953
Bishwas Baral <bishwasbaral50@gmail.com>
2025-05-20 14:12:57 +0545

    fix: small changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(_dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(_distname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
   });
 }
 
